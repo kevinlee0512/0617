f31878d6d6f5091f7ee254ddc9c9a2b6272f4414
Your Name <kevin19990512@gmail.com>
2019-06-17 13:03:52 +0800

    second

--- 0617.R
+++ 0617.R
@@ -4,7 +4,7 @@ rm(list=ls())
 library(tidyquant)
 library(timetk)
 stock_day_year<-read_tsv("data_wrangle_practice/tej_day_price_2017_2018.txt")
-
+glimpse(stock_day_year)
 #2
 price_day_year <- stock_day_year %>% 
   rename(id    = 證券代碼, 
@@ -14,6 +14,7 @@ price_day_year <- stock_day_year %>%
          cap   = `市值(百萬元)`
          )
 dim(price_day_year)
+glimpse(price_day_year)
 
 #3
 price_day_year1 <- stock_day_year %>% 
@@ -28,6 +29,7 @@ price_day_year1 <- stock_day_year %>%
   select(id, date, price) %>% 
   spread(key = id, value = price) 
 dim(price_day_year1)
+glimpse(price_day_year1)
 
 #4
 price_day_year_na <- price_day_year1 %>% 
@@ -47,6 +49,7 @@ price_day_year_na.1
 price_day_year_clear <-  price_day_year1 %>% 
   na.locf(fromLast = TRUE, na.rm=FALSE) %>%
   select(-c("2025", "6131"))
+head(price_day_year_clear)
 
 #6
 dim(price_day_year_clear)
@@ -77,3 +80,27 @@ dim(ret_mon_year.xts)
 head(ret_mon_year.xts,5)
 
 #9
+tej <- read.csv("data_wrangle_practice/tej_day_price_2017_2018.txt",col.names = TRUE)
+glimpse(tej)
+
+tej <- tej %>% select('證券代碼','簡稱','年月日','市值(百萬元)') %>%
+  rename(id    = '證券代碼', name  = '簡稱',date  = '年月日',cap   = '市值(百萬元)') %>%
+  mutate(date = Date %>%as.character %>% as.date('%Y%m%d')) %>%
+  mutate(id = id  %>% as.character) %>%
+  arrange(desc(date),desc(cap)) %>%
+  select(3,4,1,2) %>%
+  filter()
+
+mutate(diff_1 = sales_lag_1) %>%
+  mutate(pct_diff_1 = iff_1 / sales_lag_1) %>%
+  mutate(pct_diff_1_chr = scales::percent(pct_diff_1))
+
+
+#11
+ret_day_year2 <- price_day_year_clear %>% 
+  select(1:6) %>% 
+  tk_tb1(select = ) %>% 
+  Return.calculate(method = "log") %>% 
+  gather(key=id,value=price)
+
+dim(ret_day_year2)
